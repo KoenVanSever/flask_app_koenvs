1ffc14c47a79bda7bbf794d84b288d1499f29754
koenvs <koen.vansever@gmail.com>
2021-02-14 21:46:01 +0100

    update params index page filter option

--- app/static/js/param_index.js
+++ app/static/js/param_index.js
@@ -1,9 +1,15 @@
 const searchHeader = document.querySelector("#search-header");
 const searchChildren = searchHeader.children;
 const rows = document.querySelectorAll(".tr-data");
-const filterButton = document.querySelector("#filter-button");
 
-filterButton.addEventListener("click", toggle_hide_on_search)
+for (child of searchChildren) {
+    child.lastChild.addEventListener("keypress", (e) => {
+        if (e.key == "Enter") {
+            console.log("test");
+            toggle_hide_on_search();
+        }
+    });
+}
 
 function toggle_hide_on_search() {
     for (line of rows) {
@@ -15,10 +21,8 @@ function toggle_hide_on_search() {
                 continue;
             } else {
                 for (line of rows) {
-                    if (!line.children[e].innerHTML.includes(searchChildren[e].lastChild.value)) {
+                    if (!line.children[e].innerHTML.includes(searchChildren[e].lastChild.value) && line.hidden == false) {
                         line.hidden = true;
-                    } else {
-                        line.hidden = false;
                     }
                 }
             }
